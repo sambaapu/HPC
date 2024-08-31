4bd100906d2c596bf27d75f115de082b4cb64eab
Sambit Senapati <sambaapu@gmail.com>
2024-08-30 23:26:07 -0400

    HPC week1 bash assignment

---
+++ week1-bash/bash_assignment.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+output=output_bash.txt
+
+mkdir bash_workdir
+cp data.txt bash_workdir/data.txt
+cd bash_workdir
+pwd >> $output
+first_var="I Love HPC!"
+echo $first_var>>$output
+grep -i "80" data.txt>>$output
+
+second_var=$(ls)
+for eachfile in $second_var
+do
+   echo $eachfile >> $output
+done
+cp $output ../$output
+cd ..
