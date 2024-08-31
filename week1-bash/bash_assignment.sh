a73abe8f78ac5364152925b24f5e829065e0cb5e
Sambit Senapati <sambaapu@gmail.com>
2024-08-30 23:39:31 -0400

    typo changes

--- week1-bash/bash_assignment.sh
+++ week1-bash/bash_assignment.sh
@@ -6,7 +6,7 @@ mkdir bash_workdir
 cp data.txt bash_workdir/data.txt
 cd bash_workdir
 pwd >> $output
-first_var="I Love HPC!"
+first_var="I love HPC!"
 echo $first_var>>$output
 grep -i "80" data.txt>>$output
 
